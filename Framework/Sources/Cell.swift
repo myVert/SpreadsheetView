5bdac83b9a0189f7acc3f12baa25bd441b3b1afd
ghost711 <mitts.3@gmail.com>
2025-05-02 10:14:10 -0400

    Update Cell.swift

--- Framework/Sources/Cell.swift
+++ Framework/Sources/Cell.swift
@@ -98,7 +98,7 @@ open class Cell: UIView {
 }
 
 extension Cell: Comparable {
-    public static func <(lhs: Cell, rhs: Cell) -> Bool {
+    @MainActor public static func <(lhs: Cell, rhs: Cell) -> Bool {
         return lhs.indexPath < rhs.indexPath
     }
 }
